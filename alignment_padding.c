3aaa821ccd3cc0e9ad67ef90d12b148c76cb1ef9
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-03-24 20:24:45 -0700

    alignment_padding.c

--- alignment_padding.c
+++ alignment_padding.c
@@ -14,10 +14,16 @@ typedef struct {
     char z;
 }B;
 
+typedef struct {
+    char i;
+    int j;
+    int k;
+}C;
 int main(void) {
 
     A a = {'X', 67, 'Z'};
     B b = {69, 'Y', 'Z'};
+    C c = {'i', 67, 420 };
 
     printf("=== Struct A ===\n");
     printf("sizeof(struct A) = %zu\n", sizeof( A)); // total size of the struct
@@ -32,6 +38,26 @@ int main(void) {
     printf("offsetof(struct B, x) = %zu\n", offsetof(B, x));
     printf("offsetof(struct B, y) = %zu\n", offsetof(B, y));
     printf("offsetof(struct B, z) = %zu\n", offsetof(B, z));
+
+    printf("\n=== Addresses in A ===\n");
+    printf("&a      = %p\n", (void*)&a);
+    printf("&a.x    = %p\n", (void*)&a.x);
+    printf("&a.y    = %p\n", (void*)&a.y);
+    printf("&a.z    = %p\n", (void*)&a.z);
+
+    printf("\n=== Addressed in B ===\n");
+    printf("&b      = %p\n", (void*)&b);
+    printf("&b.x    = %p\n", (void*)&b.x);
+    printf("&b.y    = %p\n", (void*)&b.y);
+    printf("&b.z    = %p\n", (void*)&b.z);
+
+    printf("\n=== Addresses in C\n");
+    printf("&c      = %p\n", (void*) &c);
+    printf("&c.i      = %p\n", (void*) &c.i);
+    printf("&c.j      = %p\n", (void*) &c.j);
+    printf("&c.k      = %p\n", (void*) &c.k);
+
+
 
 
 
